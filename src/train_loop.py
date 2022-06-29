004155fa8482d6d61ad186dc65b822a85d5df2e4
Tomoya Sawada <stomoya0110@gmail.com>
2022-06-29 11:10:20 +0900

    [UP]

--- src/train_loop.py
+++ src/train_loop.py
@@ -1,10 +1,20 @@
 
 import storch
 from storch import hydra_utils
+from storch.path import Folder, Path
+
+
+def setup_folder(config, child_folders: dict):
+    folder = Folder(Path(config.ckpt_folder) / config.name / storch.get_now_string())
+    folder.add_children(**child_folders)
+    folder.mkdir()
+    return folder
 
 
 def train(config):
-    pass
+
+    folder = setup_folder(config.config.experiment, dict(model='model', image='image'))
+    hydra_utils.save_hydra_config(config, folder.root / 'config.yaml')
 
 
 def launch():
